ea8409fa9e9b80b0f252ef798deb5d673c069266
Gassan <g_fox_fts@yahoo.com>
2018-01-07 14:10:28 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+let express = require('express');
+let path = require('path');
+const mongoose = require('mongoose');
+
+//init app
+
+let app = express();
+
+//view engine setup
+
+app.set('view',path.join(__dirname,'views'));
+app.set('view engine','ejs');
+
+// set public folder
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.get('/',(reg,res)=>{
+    res.send('working');
+})
+
+//start the server 
+
+let port = 3000;
+app.listen(port,()=> console.log('Server start on port'+port));
+
