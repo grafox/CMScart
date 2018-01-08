0dabc5b187b63719e5e2c6f6dbe0edd716df1b21
Gassan <g_fox_fts@yahoo.com>
2018-01-08 13:02:38 +0100

    index.ejs

--- config/database.js
+++ config/database.js
@@ -1,3 +1,4 @@
 module.exports = {
     database : 'mongodb://localhost/cmscart'
+   // database : 'mongodb://admin:PlayWonder@10@cluster0-shard-00-00-avmol.mongodb.net:27017,cluster0-shard-00-01-avmol.mongodb.net:27017,cluster0-shard-00-02-avmol.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin'
 }
